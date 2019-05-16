7fbe5c0724989337b962cfef7221ffcc34d6652e
Ana Maria Espinosa <amespinosa@advicetec.com>
2019-05-16 11:13:40 -0500

    Cambio de permisos para acceder a las APIs

--- iotsettings/settings.py
+++ iotsettings/settings.py
@@ -91,7 +91,7 @@ REST_FRAMEWORK = {
        'rest_framework.authentication.SessionAuthentication',
    ),
    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.IsAuthenticated',
    ),
 }
 
